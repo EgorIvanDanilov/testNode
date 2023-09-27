eb956858c9e8640a9b02630808beb17d5467019b
Egor Danilov <mellowd94@gmail.com>
2023-09-27 21:10:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello,world!");
+// объявление переменных
+const x = "word";
+let y = "Another word";
+
+//Строки Strings
+let str1 = "Это строка";
+let str2 = "тоже строка";
+let str3 = `Еще одна строка`;
+
+//числа - numbers
+let num1 = 2;
+let num2 = 2.2;
+console.log(num2);
+
+// Boolean
+let boolean1 = true;
+let boolean2 = false;
+
+console.log(typeof boolean1);
+console.log(typeof num2);
+console.log(typeof str2);
+
+// undefined
+
+let smth;
+console.log(typeof smth);
+
+// Big int
+console.log(Number.MAX_SAFE_INTEGER);
